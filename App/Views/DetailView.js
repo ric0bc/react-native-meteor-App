77143106bc021200fe8fd578ce7d3e73150433b5
ric0bc <ricky.cierny@gmail.com>
2017-01-10 13:56:25 +0100

    Update DetailView.js

--- App/Views/DetailView.js
+++ App/Views/DetailView.js
@@ -4,16 +4,43 @@ import {
   Text,
   View,
   TouchableHighlight,
+  Image
 } from 'react-native';
 
 export default class DetailView extends Component {
+
+	goBack(){
+	    this.props.navigator.pop();
+  	}
+
 
 	render(){
 
 		return(
 			<View style={styles.container}>
-				<Text style={styles.text}>DetailView</Text>
-				<Text>{this.props.name}</Text>
+				<View style={styles.header}>
+		          <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+		            <TouchableHighlight
+		              onPress={this.goBack.bind(this)}
+		            >
+		              <Image
+		                source={require('../Images/left-arrow.png')}
+		                style={{ height: 35, width: 35, marginLeft: 15}}
+		              />
+		            </TouchableHighlight>
+		            <TouchableHighlight
+		              
+		            >
+		              <Image
+		                source={require('../Images/send-button.png')}
+		                style={{ height: 35, width: 35, marginRight: 15}}
+		              />
+		            </TouchableHighlight>
+		          </View>
+		        </View>
+				<View style={styles.detailPost}>
+					<Text style={styles.text}>{this.props.name}</Text>
+				</View>
 			</View>
 		)
 	}
@@ -23,13 +50,20 @@ export default class DetailView extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignSelf: 'center'
 	},
 	text: {
-		fontSize: 24
+		color: 'white',
+		marginLeft: 15,
 	}, 
-	subText: {
-
-	}
+	header: {
+	   minHeight: 60,
+	   backgroundColor: '#2196F3',
+	   justifyContent: 'center'
+	},
+	detailPost: {
+		backgroundColor: '#0097A7',
+		minHeight: 100,
+		margin: 10,
+		justifyContent: 'center',
+	},
 });
